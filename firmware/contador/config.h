22a8655367df2a55e079bebed6ca61917ab839d6
0000907919-lab <0000907919@senaimgaluno.com.br>
2026-03-09 14:29:56 -0300

    Add configuration header for WiFi and MQTT settings

---
+++ firmware/contador/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define WIFI_SSID     "seu_wifi"
+#define WIFI_PASS     "sua_senha"
+
+#define MQTT_BROKER   "192.168.1.100"
+#define MQTT_PORT     1883
+#define MQTT_USER     ""
+#define MQTT_PASS     ""
+#define MQTT_CLIENT   "esp32-contador-01"
+
+#define MQTT_TOPIC    "doceria/contador/banana-passa"
+
+#define SENSOR_PIN    18
+#define LED_PIN       2
+#define DEBOUNCE_MS   300
+#define BATCH_SIZE    1
