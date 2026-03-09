3bfba7439b29de6fc18d6d0cdfabbadb019735ba
0000907919-lab <0000907919@senaimgaluno.com.br>
2026-03-09 14:30:55 -0300

    Create config.h

---
+++ firmware/etiquetagem/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define WIFI_SSID       "seu_wifi"
+#define WIFI_PASS       "sua_senha"
+
+#define MQTT_BROKER     "192.168.1.100"
+#define MQTT_PORT       1883
+#define MQTT_USER       ""
+#define MQTT_PASS       ""
+#define MQTT_CLIENT     "esp32-etiquetagem-01"
+
+#define MQTT_TOPIC_LOTE "doceria/lote"
+
+#define BOTAO_PIN       15
+#define LED_OK_PIN      2
+#define LED_ERR_PIN     4
+
+#define PRODUTO_ID      1
+#define VALIDADE_DIAS   180
